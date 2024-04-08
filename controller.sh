bc6d9d84a996cc41e13af68cc8e89b63dc229afc
rdias66 <dias.a@live.com>
2024-04-08 12:16:53 -0300

    tcp and udp redirect funcs

--- controller.sh
+++ controller.sh
@@ -20,9 +20,17 @@ allowPort(){
   iptables -A INPUT -p tcp --dport "$port" -j ACCEPT
 }
 
-redirectToInnerPort(){
+tcpServiceRedirectTo(){
   local fromPort="$1"
   local toIp="$1"
   local toPort="$1"
   iptables -t nat -A PREROUTING -i eth0 -p tcp --dport "$fromPort" -j DNAT --to "$toIp":"$toPort"
 }
+
+udpServiceRedirectTo(){
+  local fromPort="$1"
+  local toIp="$1"
+  local toPort="$1"
+  iptables -t nat -A PREROUTING -i eth0 -p udp --dport "$fromPort" -j DNAT --to $toIp":"$toPort"
+}
+
